bcc6394ec02c3e8526ac90c00dca4dc29219bab6
Kananek Thongkam <info.dvgamer@gmail.com>
2026-07-26 19:38:08 +0700

    style(ui): format key canvas expression

--- src/components/Key.svelte
+++ src/components/Key.svelte
@@ -134,10 +134,7 @@
 	export let size = 144;
 	$: canvasWidth = renderWidth ?? size;
 	$: canvasHeight = renderHeight ?? size;
-	$: canvasStyle =
-		resolvedAppearance == "touch"
-			? "width: 160px; height: 100px;"
-			: `transform: scale(${(112 / size) * scale});`;
+	$: canvasStyle = resolvedAppearance == "touch" ? "width: 160px; height: 100px;" : `transform: scale(${(112 / size) * scale});`;
 	$: (async () => {
 		const sl = structuredClone(slot);
 		if (!sl) {
