7cd7ce292ab01c3cf455fd2fcf1945a09cfb19f8
Kananek Thongkam <info.dvgamer@gmail.com>
2026-07-26 19:36:53 +0700

    fix(tauri): initialise single-instance handling first

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -63,6 +63,23 @@ async fn main() {
 	}
 
 	let app = match Builder::default()
+		.plugin(tauri_plugin_single_instance::init(|app, args, _| {
+			if let Some(pos) = args.iter().position(|x| x.to_lowercase().trim() == "--reload-plugin") {
+				if args.len() > pos + 1 {
+					tauri::async_runtime::spawn(frontend::plugins::reload_plugin(app.clone(), args[pos + 1].clone()));
+				}
+			} else if let Some(pos) = args.iter().position(|x| x.to_lowercase().trim() == "--process-message") {
+				if args.len() > pos + 1 {
+					tauri::async_runtime::spawn(events::inbound::process_incoming_message(
+						Ok(tokio_tungstenite::tungstenite::Message::Text(args[pos + 1].clone().into())),
+						"",
+						true,
+					));
+				}
+			} else {
+				let _ = show_window(app);
+			}
+		}))
 		.invoke_handler(tauri::generate_handler![
 			frontend::restart,
 			frontend::get_devices,
@@ -282,23 +299,6 @@ If you have already donated, thank you so much for your support!"#,
 				.build(),
 		)
 		.plugin(tauri_plugin_cors_fetch::init())
-		.plugin(tauri_plugin_single_instance::init(|app, args, _| {
-			if let Some(pos) = args.iter().position(|x| x.to_lowercase().trim() == "--reload-plugin") {
-				if args.len() > pos + 1 {
-					tauri::async_runtime::spawn(frontend::plugins::reload_plugin(app.clone(), args[pos + 1].clone()));
-				}
-			} else if let Some(pos) = args.iter().position(|x| x.to_lowercase().trim() == "--process-message") {
-				if args.len() > pos + 1 {
-					tauri::async_runtime::spawn(events::inbound::process_incoming_message(
-						Ok(tokio_tungstenite::tungstenite::Message::Text(args[pos + 1].clone().into())),
-						"",
-						true,
-					));
-				}
-			} else {
-				let _ = show_window(app);
-			}
-		}))
 		.plugin(tauri_plugin_autostart::init(tauri_plugin_autostart::MacosLauncher::LaunchAgent, Some(vec!["--hide"])))
 		.plugin(tauri_plugin_dialog::init())
 		.plugin(tauri_plugin_deep_link::init())
